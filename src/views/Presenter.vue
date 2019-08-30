f715d57a97201c13b29dfca1ab61e639d11e4df2
ray-vostro-ubuntu <rayanticlimatic@gmail.com>
2019-08-30 10:30:20 +0900

    in progress

---
+++ src/views/Presenter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <v-container>
+    <v-row no-gutters>
+      <v-col>
+        <v-card tile outlined>
+        </v-card>
+      </v-col>
+    </v-row>    
+  </v-container>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  created() {
+    this.getAllPdfsForUser();
+  },
+  data: () => ({
+  }),
+  computed: {
+    ...mapState({
+    }),
+  },
+  methods: {
+    ...mapActions([
+      'getAllPdfsForUser',
+    ]),
+  },
+};
+</script>
